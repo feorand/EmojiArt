b9d3c9e52e3bedb854fd808d639badbd8cb54d51
Pavel Prokofyev <feorand@gmail.com>
2018-02-23 18:18:20 +0500

    implement basic dataSource methods

--- EmojiArt/EmojiDocumentsTableViewController.swift
+++ EmojiArt/EmojiDocumentsTableViewController.swift
@@ -8,44 +8,19 @@
 
 import UIKit
 
-class EmojiDocumentsTableViewController: UITableViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-    // MARK: - Table view data source
-
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
-    }
-
+class EmojiDocumentsTableViewController: UITableViewController
+{
+    var emojiDocuments = ["One", "Two", "Three"]
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+        return emojiDocuments.count
     }
 
-    /*
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
+        let cell = tableView.dequeueReusableCell(withIdentifier: "EmojiDocument", for: indexPath)
+        cell.textLabel?.text = emojiDocuments[indexPath.row]
         return cell
     }
-    */
 
     /*
     // Override to support conditional editing of the table view.
