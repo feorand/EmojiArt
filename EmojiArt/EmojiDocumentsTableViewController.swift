cda02a6b217ec97728b195eac79c19156ce2c6d9
Pavel Prokofyev <feorand@gmail.com>
2018-02-23 18:28:49 +0500

    implement Delete row

--- EmojiArt/EmojiDocumentsTableViewController.swift
+++ EmojiArt/EmojiDocumentsTableViewController.swift
@@ -27,15 +27,10 @@ class EmojiDocumentsTableViewController: UITableViewController
         return cell
     }
 
-    /*
-    // Override to support editing the table view.
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
-            // Delete the row from the data source
+            emojiDocuments.remove(at: indexPath.row)
             tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
+        }
     }
-    */
 }
