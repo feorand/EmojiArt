057327a76a8ad7f8001e151891f530eaef9e239d
Pavel Prokofyev <feorand@gmail.com>
2018-03-26 13:00:07 +0500

    add Input Items cell

---
+++ EmojiArt/InputItemsCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  InputItemsCollectionViewCell.swift
+//  EmojiArt
+//
+//  Created by Pavel Prokofyev on 26.03.2018.
+//  Copyright © 2018 Pavel Prokofyev. All rights reserved.
+//
+
+import UIKit
+
+class InputItemsCollectionViewCell: UICollectionViewCell {
+
+    @IBOutlet weak var textField: UITextField!
+}
