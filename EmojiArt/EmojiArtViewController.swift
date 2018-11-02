c48ffe12ed477c6b0e55f040644df55293387af1
Pavel Prokofyev <feorand@gmail.com>
2018-11-02 17:30:42 +0500

    Implement Source files hierarchy

--- EmojiArt/EmojiArtViewController.swift
+++ EmojiArt/EmojiArtViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController
+class EmojiArtViewController: UIViewController
 {
     var emojiCollectionViewController: DynamicCollectionViewController!
         
