9c6ab27a6547a47ea013d8dfc696dc48fcb681fa
Pavel Prokofyev <feorand@gmail.com>
2018-11-07 13:42:59 +0500

    implement UIDocument methods

--- EmojiArt/Document/EmojiArtDocument.swift
+++ EmojiArt/Document/EmojiArtDocument.swift
@@ -8,6 +8,37 @@
 
 import UIKit
 
+// Class representing document for project Model
 class EmojiArtDocument: UIDocument {
-
+    
+    //MARK:- Properties
+    
+    //Model for representing
+    var emojiArt: EmojiArt?
+    
+    var thumbnailImage: UIImage?
+    
+    //MARK:- UIDocument methods
+    
+    //Saving model to a document
+    override func contents(forType typeName: String) throws -> Any {
+        return emojiArt?.json() as Any
+    }
+    
+    //Loading model from a document
+    override func load(fromContents contents: Any, ofType typeName: String?) throws {
+        if let data = contents as? Data {
+            emojiArt = EmojiArt(fromJson: data)
+        }
+    }
+    
+    //Thumbnail for document
+    override func fileAttributesToWrite(to url: URL, for saveOperation: UIDocument.SaveOperation) throws -> [AnyHashable : Any] {
+        var fileAttributes = try super.fileAttributesToWrite(to: url, for: saveOperation)
+        if let thumbnail = thumbnailImage {
+            fileAttributes[URLResourceKey.thumbnailDictionaryKey] = [URLThumbnailDictionaryItem.NSThumbnail1024x1024SizeKey: thumbnail]
+        }
+        
+        return fileAttributes
+    }
 }
