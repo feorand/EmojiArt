ae859a6a6d66a2b6bacbe7fc2c4e692c4fe0149b
Pavel Prokofyev <feorand@gmail.com>
2018-11-23 11:30:58 +0500

    add StatsViewController

---
+++ EmojiArt/StatsViewController.swift
@@ -0,0 +1,13 @@
+//
+//  StatsViewController.swift
+//  EmojiArt
+//
+//  Created by Pavel Prokofyev on 23/11/2018.
+//  Copyright © 2018 Pavel Prokofyev. All rights reserved.
+//
+
+import UIKit
+
+class StatsViewController: UIViewController {
+    
+}
